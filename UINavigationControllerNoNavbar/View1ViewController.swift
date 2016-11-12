fe1d17e82610a107deef935b241b021a71814244
Alex Tang <altitude@funkware.com>
2016-11-12 08:45:39 -0800

    not using nav controller yet

---
+++ UINavigationControllerNoNavbar/View1ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  View1ViewController.swift
+//  UINavigationControllerNoNavbar
+//
+//  Created by Alex Tang on 11/12/16.
+//  Copyright © 2016 Funkware. All rights reserved.
+//
+
+import UIKit
+
+class View1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func gotTo2Tapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let vc = storyboard.instantiateViewController(withIdentifier: "View2ViewController") as? View2ViewController {
+            vc.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+            self.present(vc, animated: true) {
+            }
+        }
+    }
+}
